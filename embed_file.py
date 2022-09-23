61072f3aebe470bf1b130664de9cbe0326eb44a1
Fractal-Tess <vgfractal@gmail.com>
2022-09-24 01:37:30 +0300

    u

--- embed_file.py
+++ embed_file.py
@@ -1,7 +1,4 @@
-# Open all three files (the original cake image, the hidden executable to hide inside of the image, and the output file image )
-with open ('src/assets/original-cake.png', 'rb') as original, open('src/assets/cake.png', 'wb',) as secret:
-    # Write to the output file the original image
-    secret.write(original.read())
-    # Then, append the executable into the image
-    secret.write(b'Kind of curious if you found this through the javascript inside of the web page, or you actually viewed the source of the cake image via a hex editor, send me an SS of the hex editor for bonus points: Flag: You cannot find me!')
-    
+with open('src/assets/original-cake.png', 'rb') as original:
+    with open('src/assets/fake-cake.png', 'wb',) as secret:
+        secret.write(original.read())
+        secret.write(b'https://hentaihaven.xxx/')
--- extract_file.py
+++ extract_file.py
@@ -1,15 +1,9 @@
-# Every single png file ends with these bytes
 png_end_hex = b"\x00\x00\x00\x00\x49\x45\x4e\x44\xae\x42\x60\x82"
 
-# Open the cake file
-with open ('src/assets/cake.png', 'rb') as f:
-    # Read the png file
+with open ('src/assets/fake-cake.png', 'rb') as f:
     content = f.read()
-    # Find the content ending of the png file
     offset = content.index(png_end_hex)
-    # Move the cursor to the index
     f.seek(offset + len(png_end_hex))
-    # Print the data
     print(f.read())
     
     
